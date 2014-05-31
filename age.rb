0c5b0d107c537d9249027dc7e2850b64bc19044c
wagraham <wagraham87@gmail.com>
2014-05-30 21:18:49 -0400

    commit

---
+++ age.rb
@@ -0,0 +1,16 @@
+#output of age.rb for someone 20 yrs old
+
+puts "How Old Are you?"
+age = gets.chomp.to_i
+
+puts " in ten years you will be " 
+puts age + 10
+
+puts " in twenty years you will be " 
+puts age + 20
+
+puts " in thirty years you will be " 
+puts age + 30
+
+puts " in forty years you will be " 
+puts age + 40
