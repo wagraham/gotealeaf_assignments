77d44bc60b42362af279a9a54cf9154b4a3e8f72
wagraham <wagraham87@gmail.com>
2014-06-05 17:06:30 -0400

    first edition

---
+++ conditional3.rb
@@ -0,0 +1,21 @@
+
+puts "enter a number between 0 and 100"
+number = gets.chomp.to_i  
+
+if number < 0
+  
+  puts "Hey! That's a negative number"  
+
+elsif number <= 50
+
+  puts "#{number} is 0 and 50"
+
+elsif number <=100
+
+  puts "#{number} is between 51 and 100" 
+
+else
+
+  puts "#{number} is above 100"
+
+end
