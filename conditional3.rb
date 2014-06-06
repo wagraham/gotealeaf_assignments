37717aa950098bdbe4c6fb68b49ec97449927b8d
wagraham <wagraham87@gmail.com>
2014-06-06 08:47:46 -0400

    with case statement

--- conditional3.rb
+++ conditional3.rb
@@ -1,21 +1,33 @@
 
-puts "enter a number between 0 and 100"
-number = gets.chomp.to_i  
 
-if number < 0
+def evaluate_number(number)
   
-  puts "Hey! That's a negative number"  
+  case
 
-elsif number <= 50
+  when number < 0
+    
+    puts "Hey! That's a negative number"  
 
-  puts "#{number} is 0 and 50"
+  when number <= 50
 
-elsif number <=100
+    puts "#{number} is 0 and 50"
 
-  puts "#{number} is between 51 and 100" 
+  when number <=100
 
-else
+    puts "#{number} is between 51 and 100" 
 
-  puts "#{number} is above 100"
+  else
+
+    puts "#{number} is above 100"
+
+  end
 
 end
+
+
+
+puts "enter a number between 0 and 100"
+
+number = gets.chomp.to_i  
+
+evaluate_number(number)
