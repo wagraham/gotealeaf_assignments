678953f15cf4131b00ac069535fdf49e3cb2d4ff
wagraham <wagraham87@gmail.com>
2014-06-09 21:24:28 -0400

    exercise7

--- array.rb
+++ array.rb
@@ -1,2 +1,12 @@
-arr = [["test", "hello", "world"],["example", "mem"]]
-print arr.last.first
+
+array = [1, 2, 3, 4]
+new_array = []
+
+array.each do |n|
+  new_array << n + 2
+end
+
+p array
+p new_array
+
+
