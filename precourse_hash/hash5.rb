19fa0e1ef75cffeb9ce3887327e6e08405846df3
wagraham <wagraham87@gmail.com>
2014-06-11 20:13:32 -0400

    arg

---
+++ precourse_hash/hash5.rb
@@ -0,0 +1,18 @@
+
+my_hash = {
+     john: "beast",
+     bob: "beauty"
+}
+
+
+  if my_hash.has_value?("beast")
+    puts "this has the value of 'beast'"
+  else 
+    puts "this has does not have the value of 'beast'"
+  end
+
+
+
+
+
+
